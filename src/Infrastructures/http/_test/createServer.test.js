12ef6d3ca6fda0565c45ea13bec8a23d23355a3f
wahyudi prasatia <wahyudiprasatia3@gmail.com>
2023-11-30 11:19:34 +0700

    pass say hello world test case 3

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -29,6 +29,23 @@ describe('HTTP server', () => {
     expect(response.statusCode).toEqual(404);
   });
 
+
+  describe('when GET /', () => {
+    it('should return 200 and hello world', async () => {
+      // Arrange
+      const server = await createServer({});
+      // Action
+      const response = await server.inject({
+        method: 'GET',
+        url: '/',
+      });
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(200);
+      expect(responseJson.value).toEqual('Hello world!');
+    });
+  });
+
   describe('when POST /users', () => {
     it('should response 201 and persisted user', async () => {
       // Arrange
@@ -523,19 +540,5 @@ describe('HTTP server', () => {
   });
 
 
-  describe('when GET /', () => {
-    it('should return 200 and hello world', async () => {
-      // Arrange
-      const server = await createServer({});
-      // Action
-      const response = await server.inject({
-        method: 'GET',
-        url: '/',
-      });
-      // Assert
-      const responseJson = JSON.parse(response.payload);
-      expect(response.statusCode).toEqual(200);
-      expect(responseJson.value).toEqual('Hello world!');
-    });
-  });
+  
 });
