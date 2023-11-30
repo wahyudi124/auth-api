f9c9e17f74a1d952369bb294fcb45b2091676ace
wahyudi prasatia <wahyudiprasatia3@gmail.com>
2023-11-30 10:48:21 +0700

    pass say hello world test case 1

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -29,6 +29,8 @@ const createServer = async (container) => {
     }),
   });
 
+  
+
   server.ext('onPreResponse', (request, h) => {
     // mendapatkan konteks response dari request
     const { response } = request;
